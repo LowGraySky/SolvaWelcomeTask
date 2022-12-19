4fb13eeae7cc35825a2ac24dc80b5495de1d9fc0
NeckNecson <lowgraysky@MacBook-Pro-NeckNecson.local>
2022-12-19 21:19:12 +0600

    Initial commit

---
+++ src/main/java/kz/lowgraysky/solva/welcometask/SolvaWelcomeTaskApplication.java
@@ -0,0 +1,13 @@
+package kz.lowgraysky.solva.welcometask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvaWelcomeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvaWelcomeTaskApplication.class, args);
+	}
+
+}
