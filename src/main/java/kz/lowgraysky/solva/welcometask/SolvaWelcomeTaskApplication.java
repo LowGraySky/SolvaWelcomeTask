93f5a9b96ca92d46b9d9906487d789be80fc9bcd
lowgrayksy <nick.alexeevkz@gmail..com>
2022-12-23 23:18:23 +0600

    RestTemplate bean for remote exchange rate service

--- src/main/java/kz/lowgraysky/solva/welcometask/SolvaWelcomeTaskApplication.java
+++ src/main/java/kz/lowgraysky/solva/welcometask/SolvaWelcomeTaskApplication.java
@@ -2,12 +2,25 @@ package kz.lowgraysky.solva.welcometask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class SolvaWelcomeTaskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SolvaWelcomeTaskApplication.class, args);
-	}
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setReadTimeout(Duration.ofMillis(10000))
+                .setConnectTimeout(Duration.ofMillis(5000))
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolvaWelcomeTaskApplication.class, args);
+    }
 
 }
