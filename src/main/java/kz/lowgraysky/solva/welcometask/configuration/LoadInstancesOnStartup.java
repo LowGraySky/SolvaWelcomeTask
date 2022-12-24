9ea974d1acb3c1362acae948c553e8a6f510025d
lowgrayksy <nick.alexeevkz@gmail..com>
2022-12-24 16:28:27 +0600

    Modify startup function with new entity fields

--- src/main/java/kz/lowgraysky/solva/welcometask/configuration/LoadInstancesOnStartup.java
+++ src/main/java/kz/lowgraysky/solva/welcometask/configuration/LoadInstancesOnStartup.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 
 @Component
 public class LoadInstancesOnStartup implements CommandLineRunner {
@@ -27,10 +28,23 @@ public class LoadInstancesOnStartup implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Currency USDCurrency = new Currency("USD");
         currencyRepository.save(new Currency("KZT"));
         currencyRepository.save(new Currency("RUB"));
-        currencyRepository.save(new Currency("USD"));
-        limitRepository.save(new TransactionLimit(configProperties.SERVICE_MONTH_LIMIT(), ExpenseCategory.SERVICE));
-        limitRepository.save(new TransactionLimit(configProperties.PRODUCT_MONTH_LIMIT(), ExpenseCategory.PRODUCT));
+        currencyRepository.save(USDCurrency);
+        limitRepository.save(new TransactionLimit(
+                configProperties.SERVICE_MONTH_LIMIT() == null ?
+                        BigDecimal.ZERO : configProperties.SERVICE_MONTH_LIMIT(),
+                ExpenseCategory.SERVICE,
+                ZonedDateTime.now(),
+                USDCurrency
+        ));
+        limitRepository.save(new TransactionLimit(
+                configProperties.PRODUCT_MONTH_LIMIT() == null ?
+                        BigDecimal.ZERO : configProperties.PRODUCT_MONTH_LIMIT(),
+                ExpenseCategory.PRODUCT,
+                ZonedDateTime.now(),
+                USDCurrency
+        ));
     }
 }
