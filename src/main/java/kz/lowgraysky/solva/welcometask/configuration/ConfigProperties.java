c8f2eefe594974aa2f9ff535c1f25500037231cb
lowgrayksy <nick.alexeevkz@gmail..com>
2022-12-24 14:52:39 +0600

    Setting initial limits from application.properties

--- src/main/java/kz/lowgraysky/solva/welcometask/configuration/ConfigProperties.java
+++ src/main/java/kz/lowgraysky/solva/welcometask/configuration/ConfigProperties.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import java.math.BigDecimal;
+
 @Configuration
 @PropertySource("classpath:application.properties")
 public class ConfigProperties {
@@ -14,11 +16,25 @@ public class ConfigProperties {
     @Value("${api.twelvedata.apikey}")
     private String EXCHANGE_SERVICE_URL;
 
+    @Value("${limit.product.month_limit}")
+    private BigDecimal PRODUCT_MONTH_LIMIT;
+
+    @Value("${limit.service.month_limit}")
+    private BigDecimal SERVICE_MONTH_LIMIT;
+
     public String EXCHANGE_SERVICE_API_KEY() {
         return EXCHANGE_SERVICE_API_KEY;
     }
 
     public String EXCHANGE_SERVICE_URL() {
         return EXCHANGE_SERVICE_URL;
+    }
+
+    public BigDecimal PRODUCT_MONTH_LIMIT() {
+        return PRODUCT_MONTH_LIMIT;
+    }
+
+    public BigDecimal SERVICE_MONTH_LIMIT() {
+        return SERVICE_MONTH_LIMIT;
     }
 }
